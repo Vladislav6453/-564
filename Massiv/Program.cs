21e0cb22da34f5a16845a8f3fb119f9e6b0dd42c
Admin <Admin@CAB301-4-2024>
2025-10-06 16:33:13 +1000

    Добавьте файлы проекта.

---
+++ Massiv/Program.cs
@@ -0,0 +1,39 @@
+﻿
+
+using System.Net.Http.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.Write("Введите длину массива:");
+int length = int.Parse(Console.ReadLine());
+int[] Massiv = new int[length];
+for(int i = 0; i < Massiv.Length; i++)
+{
+    Console.Write($"Введите число номер {i+1}:");
+    Massiv[i] = int.Parse(Console.ReadLine());
+}
+Console.Write("Введите число k:");
+int k = int.Parse(Console.ReadLine());
+HttpClient client = new HttpClient();
+client.BaseAddress = new Uri("http://localhost:5186/api/");
+var result = await client.PostAsJsonAsync($"Massiv/ComplexValue3/{k}", Massiv);
+var data = await result.Content.ReadFromJsonAsync<int>();
+Console.WriteLine(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
---
+++ Массивы/Controllers/MassivController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Массивы.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MassivController : ControllerBase
+    {
+        [HttpPost("ComplexValue3/{k}")]
+        public int GetRandomTestData2(int[] massiv, int k)
+        {
+            
+            int sum = massiv.Where(s => s % k == 0).Sum();
+            return sum;
+
+        }
+    }
+}
