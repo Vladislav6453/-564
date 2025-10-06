2309b7c33e871a3497eaeb0f18d377c49a3ab123
Admin <Admin@CAB301-4-2024>
2025-10-06 18:04:34 +1000

    ноггонок7

--- Massiv/Program.cs
+++ Massiv/Program.cs
@@ -3,36 +3,131 @@
 using System.Net.Http.Json;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
+//1
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for(int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i+1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//Console.Write("Введите число k:");
+//int k = int.Parse(Console.ReadLine());
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue3/{k}", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<int>();
+//Console.WriteLine(data);
+
+//2
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for (int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i + 1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue4", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<int[]>();
+//Console.WriteLine($"Длина массива, состоящего из индексов нулей равна:{data.Length}.");
+//foreach (int i in data)
+//{
+//    Console.WriteLine(data[i]);
+//}
+//Console.ReadLine();
+
+//3
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for (int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i + 1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue5", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<bool>();
+//if (data)
+//    Console.WriteLine("Первое число отрицательно.");
+//else
+//    Console.WriteLine("Первое число положительно.");
+
+//4
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for (int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i + 1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue6", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<bool>();
+//if (data)
+//    Console.WriteLine("Массив возрастающий.");
+//else
+//    Console.WriteLine("Массив не возрастающий.");
+
+
+//5
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for (int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i + 1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue7", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<int[]>();
+//Console.WriteLine($"Длина массива, состоящего из четных чисел:{data.Length}.");
+//for(int i = 0; i < data.Length; i++)
+//{
+//    Console.WriteLine(data[i]);
+//}
+
+//6
+//Console.Write("Введите длину массива:");
+//int length = int.Parse(Console.ReadLine());
+//int[] Massiv = new int[length];
+//for (int i = 0; i < Massiv.Length; i++)
+//{
+//    Console.Write($"Введите число номер {i + 1}:");
+//    Massiv[i] = int.Parse(Console.ReadLine());
+//}
+//HttpClient client = new HttpClient();
+//client.BaseAddress = new Uri("http://localhost:5186/api/");
+//var result = await client.PostAsJsonAsync($"Massiv/ComplexValue8", Massiv);
+//var data = await result.Content.ReadFromJsonAsync<int>();
+//Console.WriteLine($"И это число равно: {data}.");
+
+
+//7
 Console.Write("Введите длину массива:");
 int length = int.Parse(Console.ReadLine());
 int[] Massiv = new int[length];
-for(int i = 0; i < Massiv.Length; i++)
+for (int i = 0; i < Massiv.Length; i++)
 {
-    Console.Write($"Введите число номер {i+1}:");
+    Console.Write($"Введите число номер {i + 1}:");
     Massiv[i] = int.Parse(Console.ReadLine());
 }
-Console.Write("Введите число k:");
+Console.Write("Введите то самое число:");
 int k = int.Parse(Console.ReadLine());
 HttpClient client = new HttpClient();
 client.BaseAddress = new Uri("http://localhost:5186/api/");
-var result = await client.PostAsJsonAsync($"Massiv/ComplexValue3/{k}", Massiv);
+var result = await client.PostAsJsonAsync($"Massiv/ComplexValue9/{k}", Massiv);
 var data = await result.Content.ReadFromJsonAsync<int>();
-Console.WriteLine(data);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine($"И это число равно: {data}.");
 
 
 
--- Массивы/Controllers/MassivController.cs
+++ Массивы/Controllers/MassivController.cs
@@ -7,13 +7,89 @@ namespace Массивы.Controllers
     [ApiController]
     public class MassivController : ControllerBase
     {
+        //1
         [HttpPost("ComplexValue3/{k}")]
         public int GetRandomTestData2(int[] massiv, int k)
         {
-            
+
             int sum = massiv.Where(s => s % k == 0).Sum();
             return sum;
 
+        }
+
+        //2
+        [HttpPost("ComplexValue4")]
+        public int[] GetRandomTestData2(int[] massiv)
+        {
+
+            int nulli = massiv.Count(s => s == 0);
+
+            int[] nulll = new int [nulli];
+            int g = 0;
+            for (int i = 0; i < massiv.Length; i++)
+            {
+                if (massiv[i] == 0)
+                {
+                    nulll[g] = i;
+                    g++;
+                }
+            }
+            return nulll;
+
+        }
+
+        //3
+        [HttpPost("ComplexValue5")]
+        public bool GetRandomTestData3(int[] massiv)
+        {
+            
+            bool Minus = massiv.First() < 0;
+            
+            return Minus;
+        }
+
+        //4
+        [HttpPost("ComplexValue6")]
+        public bool GetRandomTestData4(int[] massiv)
+        {
+            int i = massiv.First();
+            foreach(int ii in massiv)
+            {
+                if (ii >= i)
+                    i = ii;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+        //5
+        [HttpPost("ComplexValue7")]
+        public int[] GetRandomTestData7(int[] massiv)
+        {
+            int[] Chet = massiv.Where(s => s % 2 == 0).ToArray();
+
+            return Chet;
+        }
+
+        //6
+        [HttpPost("ComplexValue8")]
+        public int GetRandomTestData8(int[] massiv)
+        {
+            int Razn = massiv.Max() - massiv.Min();
+
+
+            return Razn;
+        }
+
+        //7
+        [HttpPost("ComplexValue9")]
+        public int GetRandomTestData9(int[] massiv)
+        {
+            int[] Chet = massiv.Where(s => s % 2 == 0).ToArray();
+
+
+            return Razn;
         }
     }
 }
